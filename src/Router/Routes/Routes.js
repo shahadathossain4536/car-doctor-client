b123903144b1e435374c4468b73b8b5e68379974
Shahadat Hossain <60577101+shahadathossain4536@users.noreply.github.com>
2023-03-17 08:16:40 +0600

    ..

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,8 +1,18 @@
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home/Home";
+
 const { createBrowserRouter } = require("react-router-dom");
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
